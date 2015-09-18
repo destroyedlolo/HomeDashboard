7d0c7d3aa3e53455a56952d2ae8c72cb17fe59d0
destroyedlolo <l_faillie@yahoo.com>
2015-09-18 20:50:45 +0200

    Broker connection added

--- Selenite/HDB_Config.lua
+++ Selenite/HDB_Config.lua
@@ -1,6 +1,6 @@
 -- HDB configuration file
--- all configuration change have to be made here
+-- all local configuration change have to be made here
 
 -- URL where to connect to the broker
 MQTT_URL='tcp://bpi.chez.moi:1883'
-
+MQTT_ClientID='Tablette'
