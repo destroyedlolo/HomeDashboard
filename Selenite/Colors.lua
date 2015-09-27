69b9e873cf4411b7e6b373eccfb2ded6a88af7e3
destroyedlolo <l_faillie@yahoo.com>
2015-09-27 22:33:47 +0000

    WIP

--- Selenite/Colors.lua
+++ Selenite/Colors.lua
@@ -9,3 +9,36 @@ COL_WHITE = { 0xff, 0xff, 0xff, 0xff }
 COL_BORDER = { 0x00,0x20,0xff, 0xff }
 COL_TITLE = { 0x46,0x82,0xb4, 0xff }	-- SteelBlue
 COL_DIGIT = { 0x00,0xff,0xff, 0xff }
+
+-- Find color b/w 2 known values
+-- key = known value
+-- value = corresponding color (table)
+function findcolor( val, colors )
+	if colors[val] then
+return('exact value')
+	end
+
+	local keys={}
+	for k, _ in pairs( colors ) do
+		table.insert(keys, k)
+	end
+	table.sort( keys )
+
+	local pre, post	-- previous and next color
+	for i,k in ipairs(keys) do
+		if k > val then
+			pre = keys[i-1]
+			post = k
+			break
+		end
+	end
+
+	if pre and post then -- linear function
+return('linear function')
+	elseif post then -- post without pre : 1st value
+return('post without pre : 1st value')
+	else
+return('last value')
+	end
+end
+
