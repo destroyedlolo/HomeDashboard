9ed2882a6763b0e433bb7ef7aa971720ebf80540
destroyedlolo <l_faillie@yahoo.com>
2016-04-23 20:58:34 +0000

    Broken loss reported

--- Selenite/LocalConfig.lua
+++ Selenite/LocalConfig.lua
@@ -2,5 +2,5 @@
 -- all local configuration change have to be made here
 
 MQTT_URL='tcp://bpi.chez.moi:1883' -- URL where to connect to the broker
---MQTT_URL='tcp://192.168.0.66:1883' -- URL where to connect to the broker
+--MQTT_URL='tcp://torchwood.chez.moi:1883' -- URL where to connect to the broker
 MQTT_ClientID='Tablette'	-- Broker client ID : must be unique across the system
