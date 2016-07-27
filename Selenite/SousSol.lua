7502b84badbb481fb462708c931334114455fc60
destroyedlolo <l_faillie@yahoo.com>
2016-07-27 21:50:20 +0000

    Handle "Cave" tempÃeratures

---
+++ Selenite/SousSol.lua
@@ -0,0 +1,63 @@
+-- This file define the window related to the basement "Sous sol"
+
+local window = layer:CreateWindow {
+	pos = WINTOP, size = WINSIZE,
+	caps=SelWindow.CapsConst('NONE'),
+	surface_caps=SelSurface.CapabilityConst('NONE')
+}
+window:SetOpacity(0xff)			-- Make it visible
+ss_srf = window:GetSurface()	-- Get its surface
+
+table.insert( winlist, window )
+
+-- compatibility with newer Lua
+local unpack = unpack or table.unpack
+
+-- Design
+
+ss_srf:SetColor( unpack(COL_BORDER) )
+ss_srf:SetFont( ftitle )
+ss_srf:DrawString("Sous sol", 0, 0)
+
+local goffy = ftitle:GetHeight() + 10
+
+local img,err = SelImage.create(SELENE_SCRIPT_DIR .. "/Images/SousSol.png")
+assert(img)
+local tx,ty = ss_srf:GetSize()
+img:RenderTo( ss_srf, { 20,50, tx-30, ty-70 } )
+img:destroy()	-- The image is not needed anymore
+
+srf_TCave = ss_srf:SubSurface( 450, 350, 25 + fdigit:StringWidth("-88.8°C"), fmdigit:GetHeight() )
+srf_TCave:SetColor( unpack(COL_BLACK) )
+srf_TCave:SetFont( fdigit )
+
+srf_TCaveP = ss_srf:SubSurface( 30, 350, 25 + fdigit:StringWidth("-88.8°C"), fmdigit:GetHeight() )
+srf_TCaveP:SetColor( unpack(COL_BLACK) )
+srf_TCaveP:SetFont( fdigit )
+
+-- Update functions
+function sssrfupdate()
+	ss_srf:Flip(SelSurface.FlipFlagsConst("NONE"))
+end
+
+function updateTCave()
+	UpdDataRight( srf_TCave, SelShared.get('maison/Temperature/Cave') .. "°C", { 26,123,23, 255 } )
+	SelShared.PushTask( sssrfupdate, SelShared.TaskOnceConst("LAST"))
+
+print('cave', SelShared.get( 'maison/Temperature/Cave' ))
+end
+
+function updateTCaveP()
+	UpdDataRight( srf_TCaveP, SelShared.get('maison/Temperature/CaveP') .. "°C", { 26,123,23, 255 } )
+	SelShared.PushTask( sssrfupdate, SelShared.TaskOnceConst("LAST"))
+end
+
+-- local subscription
+local ltopics = {
+	{ topic = "maison/Temperature/Cave", trigger=updateTCave, trigger_once=true },
+	{ topic = "maison/Temperature/CaveP", trigger=updateTCaveP, trigger_once=true },
+}
+
+TableMerge( Topics, ltopics)
+
+sssrfupdate()
