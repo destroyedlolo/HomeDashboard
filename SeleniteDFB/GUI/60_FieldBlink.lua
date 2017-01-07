d28dbeab36a149a0a0e1e913cb26de7a3aaff65a
destroyedlolo <l_faillie@yahoo.com>
2017-01-07 18:32:21 +0100

    Improve Blink

--- SeleniteDFB/GUI/60_FieldBlink.lua
+++ SeleniteDFB/GUI/60_FieldBlink.lua
@@ -14,46 +14,51 @@ function FieldBlink(
 )
 	local self = Field( psrf, x,y, font, align, color, ctxt, szx, szy, bgcolor )
 
-	local parent_update = self.update -- update function from the parent
+	-- Backup of parent functions
+	local parent_update = self.update
+	local parent_setColor = self.setColor
+	local parent_setColorRGB = self.setColorRGB
+
 	local val -- value to display
 	local cur_r,cur_g,cur_b,cur_a -- Current color
+	local cons_r,cons_g,cons_b,cons_a -- Consign color
 
 	local function fade()
-		local r,g,b,a = color.get()
+		cur_r = math.max( cons_r, cur_r - 10 )
+		cur_g = math.max( cons_g, cur_g - 10 )
+		cur_b = math.max( cons_b, cur_b - 10 )
+		cur_a = math.max( cons_a, cur_a - 10 )
 
-		if r < cur_r then
-			cur_r = math.max( r, cur_r - 10 )
-		end
-
-		if g < cur_g then
-			cur_g = math.max( g, cur_g - 10 )
-		end
-
-		if b < cur_b then
-			cur_b = math.max( b, cur_b - 10 )
-		end
-
-		if a < cur_a then
-			cur_a = math.max( a, cur_a - 10 )
-		end
-
-		if cur_r - r + cur_g - g + cur_b - b + cur_a - a ~= 0 then	-- Fading
-			self.setColorRGB( cur_r,cur_g,cur_b,cur_a )
+		if cur_r - cons_r + cur_g - cons_g + cur_b - cons_b + cur_a - cons_a ~= 0 then	-- Fading
+			parent_setColorRGB( cur_r, cur_g, cur_b, cur_a )
 		else
-			self.setColor( color )
+			parent_setColorRGB( cons_r, cons_g, cons_b, cons_a )
+			animTimer.TaskOnceRemove( fade )
 		end
 
 		parent_update( val )
 	end
 
+	-- Overloading
+	function self.setColor( color )
+		cons_r,cons_g,cons_b,cons_a = color.get()
+	end
+
+	function self.setColorRGB( r,g,b,a )
+		cons_r,cons_g,cons_b,cons_a = r,g,b,a
+	end
+
 	function self.update( v )
 		cur_r,cur_g,cur_b,cur_a = COL_WHITE.get()
 		val = v
-		self.setColorRGB( cur_r,cur_g,cur_b,cur_a )
+		parent_setColorRGB( cur_r,cur_g,cur_b,cur_a )
 		parent_update(v)
 
 		animTimer.TaskOnceAdd( fade )
 	end
+
+	-- initialisation
+	self.setColor(color) -- because the constructor one hasn't been saved
 
 	return self
 end
