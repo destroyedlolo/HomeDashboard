6e8079f01fec72ad6a9ef796707902112cd72e92
destroyedlolo <l_faillie@yahoo.com>
2017-01-12 22:13:58 +0000

    Starting with UPS value

---
+++ SeleniteDFB/Inputs/40_UPS.lua
@@ -0,0 +1,31 @@
+-- Manage UPS inputs
+
+function UPSdata( name, topic, 
+	func, 			-- validation function
+	topicnominal,	-- topic for nominal power
+	srf,	-- surface to display the value
+	gradient	-- gradient to colorize
+)
+	local self = MQTTinput( name, topic, func )
+	local nominal = MQTTinput( name .." nominal", topicnominal)
+
+	function self.update()
+		local v = self.get()
+		local maxp = tonumber( SelShared.get('onduleur/ups.realpower.nominal') )
+
+		if gradient then
+			srf.setColorRGB( gradient.findgradientcolor(v) )
+		end
+
+		if maxp then	-- we can calculate the real power
+			srf.update( string.format('%3.1f', v*maxp/100) .. ' W' )
+		else
+			srf.update( v .. '%' )
+		end
+	end
+
+	-- init
+	self.TaskOnceAdd( self.update )
+
+	return self
+end
