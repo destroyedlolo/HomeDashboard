cb60e57395b3345b73ababd893c3ae2fc9d4688a
destroyedlolo <l_faillie@yahoo.com>
2017-01-22 23:28:03 +0000

    Test Gauge

---
+++ SeleniteDFB/GUI/30_GaugePercent.lua
@@ -0,0 +1,42 @@
+-- Percentage Gauge
+
+function GaugePercent(
+	psrf,	-- mother surface
+	sx,sy,	-- position in the mother surface
+	sw,sh,	-- its size
+	color,	-- graph color
+	bgcolor, -- background color
+	opts
+)
+--[[ known options  :
+--	border_color : border color (if not set, no border)
+--	vertical : if set, the gauge is vertical
+--]]
+	if not opts then
+		opts = {}
+	end
+
+	local self = SubSurface(psrf, sx,sy, sw,sh )
+
+	self.setColor(color)
+
+	function self.Clear()
+		self.get():Clear( bgcolor.get() )
+	end
+
+	function self.Draw( v )
+		self.Clear()
+		
+		self.ColorApply()	-- restore foreground color (if changed while drawing border)
+		self.get():FillRectangle( 0,0, self.get():GetWidth()*v/100, self.get():GetHeight() )
+
+		if opts.border_color then
+			self.get():SetColor( opts.border_color.get() )
+			self.get():DrawRectangle(0,0, self.get():GetWidth(), self.get():GetHeight())
+		end
+	
+		self.refresh()
+	end
+
+	return self
+end
--- SeleniteDFB/HDB/80_LeftBar.lua
+++ SeleniteDFB/HDB/80_LeftBar.lua
@@ -93,7 +93,8 @@ local function f()
 		sample_text = "888.8W"
 	} )
 	x = srf_onduleur.get():GetWidth()
-	local srf_gaugeOnduleur = GaugeHPercentBg( srf, x+4, offy+8, w-x-8, srf_onduleur.get():GetHeight()-16, COL_GFXBG, COL_BORDER )
+--	local srf_gaugeOnduleur = GaugeHPercentBg( srf, x+4, offy+8, w-x-8, srf_onduleur.get():GetHeight()-16, COL_GFXBG, COL_BORDER )
+	local srf_gaugeOnduleur = GaugePercent( srf, x+4, offy+8, w-x-8, srf_onduleur.get():GetHeight()-16, COL_RED, COL_GFXBG, { border_color = COL_BORDER } )
 	local onduleur = UPSdata('UPS', 'onduleur/ups.load', 'onduleur/ups.realpower.nominal', srf_onduleur, srf_gaugeOnduleur)
 	offy = offy + srf_onduleur.get():GetHeight() + 4
 
