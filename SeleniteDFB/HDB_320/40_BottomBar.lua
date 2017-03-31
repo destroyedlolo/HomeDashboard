57829e684ecec45e3954df88311f23016625c14b
destroyedlolo <l_faillie@yahoo.com>
2017-03-31 19:17:47 +0200

    Add network notification

--- SeleniteDFB/HDB_320/40_BottomBar.lua
+++ SeleniteDFB/HDB_320/40_BottomBar.lua
@@ -2,11 +2,15 @@
 
 local function f()
 	local sw = psrf:GetWidth() - LBw
-	local sh = fstxt:GetHeight()*4
+	local sh = fstxt:GetHeight()*3
 
 	local self = SubSurface( psrf, LBw, psrf:GetHeight()-sh, sw, sh )
 	local sw, sh = self.get():GetSize()
 	local srf = self.get()
+
+	local network = ImageFiltreSurface( srf, 0,0, SELENE_SCRIPT_DIR .. "/Images/Network.png" )
+	condition_network = Condition( network )
+	table.insert( condlist, condition_network )
 
 --[[
 	local self = TextArea( psrf, 
