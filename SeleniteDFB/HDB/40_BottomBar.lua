b82ec3da8046e01cfd03ff2776e9021f6fe547ff
destroyedlolo <l_faillie@yahoo.com>
2017-04-20 20:05:09 +0000

    Change bPI's topics

--- SeleniteDFB/HDB/40_BottomBar.lua
+++ SeleniteDFB/HDB/40_BottomBar.lua
@@ -39,7 +39,7 @@ local function f()
 		ndecimal = 2,
 	} )
 
-	local loadbPI = MQTTStoreGfx( 'bPI', 'Load/bPI/1', loadbPI, srf_trndbPI, 
+	local loadbPI = MQTTStoreGfx( 'bPI', 'Machines/bPI/Load/1', loadbPI, srf_trndbPI, 
 		{
 			smax = mloadbPI,
 			gradient = Gradient(
