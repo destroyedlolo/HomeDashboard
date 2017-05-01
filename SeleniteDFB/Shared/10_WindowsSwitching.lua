c156172ff6cebe9f56177a45f6d31e1675f40c7a
destroyedlolo <l_faillie@yahoo.com>
2017-05-01 22:33:34 +0200

    Make windows management shared

--- SeleniteDFB/Shared/10_WindowsSwitching.lua
+++ SeleniteDFB/Shared/10_WindowsSwitching.lua
@@ -4,14 +4,34 @@ winlist = {}		-- Collection of windows to be slided
 
 -- Windows cycling
 local wcnt = 0, err
-function switchwindows()
+function nextWindow( force )
 	if #winlist ~= 0 then
 		wcnt = (wcnt + 1) % #winlist
 		winlist[ (wcnt % #winlist)+1 ]:RaiseToTop()
 	end
+
+	if force then
+		Notification.Log('>> ' .. wcnt)
+		switchtimer:Reset()
+	end
 end
 
-switchtimer,err = SelTimer.create { when=10, interval=10, clockid=SelTimer.ClockModeConst("CLOCK_MONOTONIC"), task=switchwindows}
+function prevWindow( force)
+	if #winlist ~= 0 then
+		wcnt = wcnt - 1	
+		if wcnt < 0 then
+			wcnt = #winlist - 1
+		end
+		winlist[ (wcnt % #winlist)+1 ]:RaiseToTop()
+	end
+
+	if force then
+		Notification.Log('<< ' .. wcnt)
+		switchtimer:Reset()
+	end
+end
+
+switchtimer,err = SelTimer.create { when=10, interval=10, clockid=SelTimer.ClockModeConst("CLOCK_MONOTONIC"), task=nextWindow}
 if err then
 	print(err)
 	return
@@ -25,17 +45,8 @@ local function handlekeys()
 
 	if SelEvent.TypeName(tp) == 'KEY' and v == 1 then
 		if SelEvent.KeyName(c) == 'VOLUMEDOWN' then
-			wcnt = wcnt - 1	
-			if wcnt < 0 then
-				wcnt = #winlist - 1
-			end
-			Notification.Log('<< ' .. wcnt)
-			winlist[ (wcnt % #winlist)+1 ]:RaiseToTop()
-			switchtimer:Reset()
 		elseif SelEvent.KeyName(c) == 'VOLUMEUP' then
-			Notification.Log('>> ' .. wcnt)
-			switchwindows()
-			switchtimer:Reset()
+			nextWindow(force)
 		elseif SelEvent.KeyName(c) == 'SEARCH' then
 			Notification.Log('Not implemented ... yet')
 		end
