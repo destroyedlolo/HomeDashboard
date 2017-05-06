ff6b679966bb069f0a8c3579bf98dd40b61a057b
destroyedlolo <l_faillie@yahoo.com>
2017-05-06 20:07:34 +0200

    Movement working in popup list

--- SeleniteDFB/HDB_320/60_ConfigMenu.lua
+++ SeleniteDFB/HDB_320/60_ConfigMenu.lua
@@ -13,9 +13,34 @@ function ConfigMenu()
 	},
 	{
 		keysactions = 'keysactions',	-- Active keysactions table
+		bordercolor = COL_LIGHTGREY
 	})
 
-	popup.setKeysActions { 
+	popup.setColor( COL_WHITE )
+	popup.get():SetFont( ftitle1 )
+	popup.get():DrawString("Configuration", 0,0)
+	popup.refresh()
+
+	local lst = vsList( popup, 
+		0, ftitle1:GetHeight() + 15, popup.get():GetWidth(),
+		{
+			{ 'Mode générale', nil },
+			{ ' > Mode Enfants', nil },
+			{ ' > > Mode Oceane', nil },
+			{ ' > > Mode Joris', nil },
+			{ ' > Mode Parents', nil }
+		},
+		fmdigit,
+		{
+			unselcolor = COL_TITLE,
+			selcolor = COL_DIGIT
+		}
+	)
+
+	popup.setKeysActions {
+			['KEY/VOLUMEDOWN/1'] = lst.selprev,
+			['KEY/VOLUMEUP/1'] = lst.selnext,
 			['KEY/POWER/1'] = popup.close
 	}
+
 end
