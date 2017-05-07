9c41a13b517b2fd2fdad9beca91b0ea041a09893
destroyedlolo <l_faillie@yahoo.com>
2017-05-07 18:23:00 +0200

    Upstaire implemented

---
+++ SeleniteDFB/HDB_320/30_MinorTempArea.lua
@@ -0,0 +1,67 @@
+-- Display a minor temperature graphics
+
+function MinorTempArea(
+	srf,	-- parent surface
+	name, topic,
+	x,y,	-- top left of this area
+	opts
+)
+--[[ known options  :
+	font : font to use to display (default fdigit)
+	title : title to display
+	size : size of the area
+
+	Ether title or size have to be present
+--]]
+	if not opts then
+		opts = {}
+	end
+	if not opts.font then
+		opts.font = fdigit
+	end
+	if opts.title then
+		srf:SetColor( COL_TITLE.get() )
+		srf:SetFont( ftitle1 )
+		srf:DrawString(opts.title, x, y)
+		if not opts.size then
+			opts.size = ftitle1:StringWidth(opts.title)
+		end
+		y = y + ftitle1:GetHeight()
+	end
+	if not opts.gradient then
+		opts.gradient = GRD_TEMPERATURE
+	end
+
+	local srf_Temp = FieldBlink( srf, animTimer,
+		x + opts.size, y, opts.font, COL_DIGIT, {
+		align = ALIGN_FRIGHT,
+		gradient = opts.gradient,
+		sample_text = "-88.8"
+	})
+	_,y = srf_Temp.get():GetBelow()
+
+	local srf_gfx = GfxArea( srf, x, y, opts.size, HSGRPH, COL_TRANSPARENT, COL_GFXBG, { align=ALIGN_RIGHT, min_delta = opts.min_delta } )
+	srf_gfx.get():FillGrandient { TopLeft={20,20,20,255}, BottomLeft={20,20,20,255}, TopRight={255,100,32,255}, BottomRight={32,255,32,255} }
+	srf_gfx.FrozeUnder()
+
+	local self = MQTTStoreGfx( name, topic, srf_Temp, srf_gfx, 
+		{
+			forced_min = 15,
+		}
+	)
+	table.insert( savedcols, self )
+
+	function self.getBelow()
+		return srf_gfx.ownsrf():GetBelow()
+	end
+
+	function self.getAfter()
+		return x + opts.size
+	end
+
+	function self.getSize()
+		return opts.size
+	end
+
+	return self
+end
--- SeleniteDFB/HDB_320/60_1er.lua
+++ SeleniteDFB/HDB_320/60_1er.lua
@@ -21,14 +21,56 @@ local function upstaire()
 	img:RenderTo( srf, { 10, WINSIZE[2] - y*s - 10, x*s, y*s } )
 	img:destroy()	-- The image is not needed anymore
 
-	local tgn = MinorTempArea( srf, 'TGN', 'maison/Temperature/Grenier Nord', 0,80,
+	local tgn = MinorTempArea( srf, 'TGN', 'maison/Temperature/Grenier Nord', 0,85,
 		{
 			font = fmdigit,
 			title = "Gr Nord"
 		}
 	)
 	srf:SetColor( COL_WHITE.get() )
-	srf:DrawLine( tgn.getAfter()+3, 80, tgn.getAfter()+3, 155 )
+	srf:DrawLine( tgn.getAfter()+2, 87, tgn.getAfter()+3, 155 )
+
+	local tcj = RoomArea( srf, 'TCJ', 'maison/Temperature/Chambre Joris', 45,40,
+		'Majordome/Mode/Joris',
+		'Majordome/HLever/Joris',
+		{
+			font = fmdigit,
+			title = "Ch Joris"
+		}
+	)
+	srf:SetColor( COL_WHITE.get() )
+	srf:DrawLine( tcj.getAfter()+2 , 43, tcj.getAfter()+2, 160 )
+
+	local tcp = RoomArea( srf, 'TCP', 'maison/Temperature/Chambre Parents', 85,25,
+		'Majordome/Mode/Parents',
+		'Majordome/HLever',
+		{
+			font = fmdigit,
+			title = "Parents"
+		}
+	)
+	srf:SetColor( COL_WHITE.get() )
+	srf:DrawLine( tcp.getAfter()+2 , 28, tcp.getAfter()+2, 130 )
+
+	RoomArea( srf, 'TCO', 'maison/Temperature/Chambre Oceane', 126,40,
+		'Majordome/Mode/Oceane',
+		'Majordome/HLever/Oceane',
+		{
+			font = fmdigit,
+			title = "Océane"
+		}
+	)
+	srf:SetColor( COL_WHITE.get() )
+	srf:DrawLine( 123, 43, 123, 160 )
+
+	MinorTempArea( srf, 'TGS', 'maison/Temperature/Grenier Sud', 165,75,
+		{
+			font = fmdigit,
+			title = "Gr. Sud"
+		}
+	)
+	srf:SetColor( COL_WHITE.get() )
+	srf:DrawLine( 163, 78, 163, 155 )
 
 	srf:Flip(SelSurface.FlipFlagsConst("NONE"))
 	return self
