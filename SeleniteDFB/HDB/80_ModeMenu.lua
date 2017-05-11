1448a30ee04bfc2732e6dc5d820a898405c92dd4
destroyedlolo <l_faillie@yahoo.com>
2017-05-11 22:53:06 +0000

    Work on menu

---
+++ SeleniteDFB/HDB/80_ModeMenu.lua
@@ -0,0 +1,41 @@
+-- Top config menu
+
+-- Unlike in other source files, this is not an object :
+-- This function creates the windows (and sub object) and then everything
+-- is managed through keys' actions.
+
+function ModeMenu( parent )
+	local menu = Menu( {
+		pos = {310,60},
+		caps=SelWindow.CapsConst('NONE'),
+		surface_caps=SelSurface.CapabilityConst('NONE')
+	},
+	{
+		{ 'Mode générale', nil },
+		{ ' > Mode Enfants', nil },
+		{ ' > > Mode Oceane', nil },
+		{ ' > > Mode Joris', nil },
+		{ ' > Mode Parents', nil }
+	}, -- list
+	fmdigit,
+	{
+		keysactions = 'keysactions',	-- Active keysactions table
+		bordercolor = COL_LIGHTGREY,
+
+		title = 'Configuration',
+		titlefont = ftitle1,
+		titlecolor = COL_WHITE,
+
+		unselcolor = COL_TITLE,
+		selcolor = COL_DIGIT,
+
+		userdata = parent
+	})
+
+	menu.setKeysActions {
+			['KEY/VOLUMEDOWN/1'] = menu.selnext,
+			['KEY/VOLUMEUP/1'] = menu.selprev,
+			['KEY/POWER/1'] = menu.close
+	}
+
+end
