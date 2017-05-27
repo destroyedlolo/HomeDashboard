d6ca13bc8e643fd259ae1c4fc71ed103f7a35f7e
destroyedlolo <l_faillie@yahoo.com>
2017-05-27 21:29:51 +0200

    Work on energy saving

--- SeleniteDFB/HDB_320/60_Econnomie.lua
+++ SeleniteDFB/HDB_320/60_Econnomie.lua
@@ -2,7 +2,28 @@
 local function energy()
 	local self = {}
 
-	cdt = MQTTCounterStat('Stat mensuelle', 'Domestik/Electricite/Mensuel')
+	local window = layer:CreateWindow {
+		pos = WINTOP, size = WINSIZE,
+		caps=SelWindow.CapsConst('NONE'),
+		surface_caps=SelSurface.CapabilityConst('NONE'),
+	}
+	window:SetOpacity(0xff)	-- Make the window visible
+	table.insert( winlist, window )
+	local srf = window:GetSurface()
+
+	srf:SetColor( COL_TITLE.get() )
+	srf:SetFont( ftitle )
+	srf:DrawString("Econnomies d'énergie", 0, 0)
+
+	cdt = MQTTCounterStatGfx(srf,
+		'Stat mensuelle', 'Domestik/Electricite/Mensuel', 
+		5, ftitle:GetHeight()+5, 200,150, {
+			bordercolor = COL_GREY ,
+			consumption_border = COL_ORANGED,
+			production_border = COL_GREEND,
+			maxyears = 3
+		} 
+	)
 
 	return self
 end
