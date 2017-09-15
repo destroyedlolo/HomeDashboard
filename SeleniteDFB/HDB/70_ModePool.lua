2bff7d3e621a6d0c4992272d5bf63f45b07a3a9b
destroyedlolo <l_faillie@yahoo.com>
2017-09-15 23:53:28 +0000

    Add Pool control

---
+++ SeleniteDFB/HDB/70_ModePool.lua
@@ -0,0 +1,37 @@
+-- Pool's modes sub menu
+
+function ModePool( titre, topic, y )
+-- topic : topic to send value
+-- y : position of this sub menu
+	local menu = Menu( {
+		pos = {350,y},
+	},
+	{
+		{ 'Heures Creuses', function () Brk:Publish( topic, 'Heures Creuses', true ) end },
+		{ 'Arret', function () Brk:Publish( topic, 'Arret', true ) end },
+		{ 'Forcé', function () Brk:Publish( topic, 'Forcé', true ) end },
+	}, -- list
+	fmenu,
+	{
+		keysactions = 'keysactions',	-- Active keysactions table
+		bordercolor = COL_LIGHTGREY,
+
+		title = titre,
+		titlefont = ftitle1,
+		titlecolor = COL_WHITE,
+
+		unselcolor = COL_TITLE,
+		selcolor = COL_DIGIT,
+		default = SelShared.get(topic),
+
+		userdata = parent
+	})
+
+	menu.setKeysActions {
+		['KEY/VOLUMEDOWN/1'] = menu.selnext,
+		['KEY/VOLUMEUP/1'] = menu.selprev,
+		['KEY/POWER/1'] = menu.close,
+		['KEY/SEARCH/1'] = menu.action
+	}
+end
+
