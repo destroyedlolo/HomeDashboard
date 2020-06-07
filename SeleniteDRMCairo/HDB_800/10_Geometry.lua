de1c0e1bfd2b0f3cad6672f52f5083c2fec32c92
destroyedlolo <l_faillie@yahoo.com>
2020-06-07 23:38:33 +0000

    Enlarge graphics size

--- SeleniteDRMCairo/HDB_800/10_Geometry.lua
+++ SeleniteDRMCairo/HDB_800/10_Geometry.lua
@@ -1,7 +1,7 @@
 -- Global geometry of the display
 
 LBw = 160 -- Left bar width
-HSGRPH = 30	-- Hight of small graphs
+HSGRPH = 60	-- Height of small graphs
 NotLine = 4	-- Nbre of lines in notification area
 BBh = fonts.stxt.size * NotLine	-- BottomBar's hight
 
