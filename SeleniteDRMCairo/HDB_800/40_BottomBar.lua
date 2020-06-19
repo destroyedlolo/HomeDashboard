f12a44047536f72b450193ea08c236c2bd6b6970
destroyedlolo <l_faillie@yahoo.com>
2020-06-20 01:30:25 +0200

    Correct top border

--- SeleniteDRMCairo/HDB_800/40_BottomBar.lua
+++ SeleniteDRMCairo/HDB_800/40_BottomBar.lua
@@ -5,41 +5,41 @@ local function f()
 	local offx=0,offy
 
 	self.setColor( COL_BORDER )
-	srf:DrawLine( 0, sh, sw, sh )
+	srf:DrawLine( 0,0 , sw,0 )
 
-	local lvdonotif = ImageFiltreSurface( self, offx, 24, SELENE_SCRIPT_DIR .. "/Images/Mail.png" )
+	local lvdonotif = ImageFiltreSurface( self, offx, 25, SELENE_SCRIPT_DIR .. "/Images/Mail.png" )
 	condition_lvdo = Condition( lvdonotif, 0 )
-	local poolnotif = ImageFiltreSurface( self, offx, 0, SELENE_SCRIPT_DIR .. "/Images/Piscine.png" )
+	local poolnotif = ImageFiltreSurface( self, offx, 1, SELENE_SCRIPT_DIR .. "/Images/Piscine.png" )
 	condition_pool = Condition( poolnotif, 0, {
 		ok_color = COL_DARKGREEN,
 		issue_color = COL_DARKRED
 	} )
 	offx = offx + 24
 
-	local network = ImageFiltreSurface( self, offx,24, SELENE_SCRIPT_DIR .. "/Images/Network.png" )
+	local network = ImageFiltreSurface( self, offx,25, SELENE_SCRIPT_DIR .. "/Images/Network.png" )
 	condition_network = Condition( network, .25, { issue_color=COL_RED } )
 	table.insert( additionnalevents, condition_network.getTimer() )
 
-	local freeboxicn = ImageFiltreSurface( self, offx,0, SELENE_SCRIPT_DIR .. "/Images/FreeboxL.png" )
+	local freeboxicn = ImageFiltreSurface( self, offx,1, SELENE_SCRIPT_DIR .. "/Images/FreeboxL.png" )
 	condition_freebox = Condition(freeboxicn, .5, { autorecover=true, issue_color=COL_RED } )
 	table.insert( additionnalevents, condition_freebox.getTimer() )
 	offx = offx + 24
 
-	local WiFi = ImageFiltreSurface( self, offx,24, SELENE_SCRIPT_DIR .. "/Images/WiFi.png" )
+	local WiFi = ImageFiltreSurface( self, offx,25, SELENE_SCRIPT_DIR .. "/Images/WiFi.png" )
 	condition_WiFi = Condition(WiFi, .5, { autorecover=true, issue_color=COL_RED } )
 	table.insert( additionnalevents, condition_WiFi.getTimer() )
 	local WiFiNoStations = MQTTinput('WiFiNoStations', 'ESPRouter_Domo/NoStations', nil, { condition=condition_WiFi, watchdog=wdWiFi } )
 
-	local barriere = ImageFiltreSurface( self, offx,0, SELENE_SCRIPT_DIR .. "/Images/BarrierePoule.png" )
+	local barriere = ImageFiltreSurface( self, offx,1, SELENE_SCRIPT_DIR .. "/Images/BarrierePoule.png" )
 	condition_BarrierePoule = Condition( barriere, 0, {issue_color=COL_RED } )
 	offx = offx + 24
 
-	Notification = NotificationArea( self, offx, 0, 200, sh, fonts.stxt, COL_DARKGREEN, { bgcolor=COL_GFXBG } )
+	Notification = NotificationArea( self, offx, 1, 200, sh, fonts.stxt, COL_DARKGREEN, { bgcolor=COL_GFXBG } )
 	local log = MQTTLog('messages', 'messages', Notification, { udata=-1 } )
 	log.RegisterTopic('messages', 'messages/Erreur', { udata=3 } )
 	offx = offx + 200
 
-	Saison( self, 'Saison', 'Majordome/Saison', offx, 0 )
+	Saison( self, 'Saison', 'Majordome/Saison', offx, 1 )
 
 	-- Drawing finished and alway visible
 	self.Visibility(true)
