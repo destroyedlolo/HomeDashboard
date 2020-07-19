3bd4bb9432ce21f782344b1b45cb4e9ed9d86147
destroyedlolo <l_faillie@yahoo.com>
2020-07-20 01:46:14 +0200

    Improve graphisms

--- SeleniteDRMCairo/HDB_800/70_Automatisme.lua
+++ SeleniteDRMCairo/HDB_800/70_Automatisme.lua
@@ -12,6 +12,11 @@ local function automatisme()
 		print("*E*",err)
 		os.exit(EXIT_FAILURE)
 	end
+	local MajordomeImg,err = SelDCSurfaceImage.createFromPNG(SELENE_SCRIPT_DIR .. "/Images/Majordome.png")
+	if not MajordomeImg then
+		print("*E*",err)
+		os.exit(EXIT_FAILURE)
+	end
 
 	function self.Clear()
 		-- Redraw window's background
@@ -20,11 +25,12 @@ local function automatisme()
 		self.setFont( fonts.title )
 		self.get():DrawStringTop("Automatismes :", 5,0 )
 		self.get():Blit(mainframe, 20,50)
+		self.get():Blit(MajordomeImg, 535,84)
 	end
 
 	self.Clear()
 
-	local MajordomeTxt = NotificationArea( self, WINSIZE.w/2 + 20, 50, WINSIZE.w/2 - 40, 250, fonts.stxt, COL_LIGHTGREY, 
+	local MajordomeTxt = NotificationArea( self, WINSIZE.w - 245, 10, 245, 205, fonts.stxt, COL_LIGHTGREY, 
 		{ 
 			bgcolor=COL_TRANSPARENT60,
 			transparency=true,
