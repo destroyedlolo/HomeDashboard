66b53013aee9c13b202f19cd295eae7aea0cccda
destroyedlolo <l_faillie@yahoo.com>
2020-09-14 01:41:28 +0200

    Add energy trends

--- SeleniteDRMCairo/HDB/70_SousSol.lua
+++ SeleniteDRMCairo/HDB/70_SousSol.lua
@@ -59,20 +59,58 @@ local function soussol()
 	---
 	--Electricity
 	---
+	local w = WINSIZE.w - 775
 
 	MQTTCounterStatGfx( self,
 		'Stat mensuelle', 'Domestik/Electricite/Mensuel', 
-		760,30 , 310,160, {
+		760,30 , WINSIZE.w - 775,175, {
 			bordercolor = COL_GREY ,
 			consumption_border = COL_ORANGED,
 			xproduction_border = COL_GREEND,
-			maxyears = 3,
-			fadeyears = 20,
+			maxyears = 5,
+			fadeyears = 5,
 			barrespace = 0,
-			yearXoffset = 3,
+			yearXoffset = 6,
 			production_offset = 2
 		} 
 	)
+
+	w = w-10
+	local srf_ctrndconso = GfxArea( self, 760, 210, w/2, 85, COL_TRANSPARENT, COL_GFXBG,{
+		heverylines={ {1000, COL_DARKGREY} },
+		vlinesH=COL_DARKGREY,
+		vlinesD=COL_GREY,
+		align=ALIGN_RIGHT
+	} )
+
+	local conso2 = MQTTStoreGfx( 'consommation2', 'TeleInfo/Consommation/values/PAPP', nil, srf_ctrndconso, 
+		{
+			forced_min = 0,
+			smax = maxTConso,
+			force_max_refresh = true,
+			group = 6*60*60 / srf_ctrndconso.get():GetWidth()	-- 6h retention
+		}
+	)
+	table.insert( savedcols, conso2 )
+
+	local srf_ctrndprod = GfxArea( self, 770 + w/2, 210, w/2, 85, COL_TRANSPARENT, COL_GFXBG,{
+		heverylines={ {1000, COL_DARKGREY} },
+		vlinesH=COL_DARKGREY,
+		vlinesD=COL_GREY,
+		align=ALIGN_RIGHT 
+	} )
+
+	local prod2 = MQTTStoreGfx( 'production2', 'TeleInfo/Production/values/PAPP', nil, srf_ctrndprod, 
+		{
+			smax = maxTProd,
+			force_max_refresh = true,
+			forced_min = 0,
+			group = 6*60*60 / srf_ctrndprod.get():GetWidth()	-- 6h retention
+		}
+	)
+	table.insert( savedcols, prod2 )
+
+	SelLog.log("*I* Consummation / Production grouped by ".. 6*60*60 / srf_ctrndconso.get():GetWidth())
 
 	---
 
