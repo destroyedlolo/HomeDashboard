a3738d137af88924f36f144360a4f6f5529cb7fe
destroyedlolo <l_faillie@yahoo.com>
2021-10-27 02:51:26 +0200

    Avoid client name classes

--- SeleniteDRMCairo/LocalConfig.lua
+++ SeleniteDRMCairo/LocalConfig.lua
@@ -3,7 +3,12 @@
 
 MQTT_URL='tcp://bpi.chez.moi:1883' -- URL where to connect to the broker
 -- MQTT_URL='tcp://torchwood.chez.moi:1883' -- URL where to connect to the broker
-MQTT_ClientID='bpi-dev'	-- Broker client ID : must be unique across the system
+MQTT_ClientID='bpi-devFB'	-- Broker client ID : must be unique across the system
+
+MAJORDOME = 'Majordome.dev'
+MARCEL = 'Marcel.test'
+--MAJORDOME = 'Majordome'
+--MARCEL = 'Marcel.prod'
 
 -- Activate for debugging only
 local _,err = SelLog.init('/tmp/HDB.log_' .. os.date('%Y%m%d'), false)
