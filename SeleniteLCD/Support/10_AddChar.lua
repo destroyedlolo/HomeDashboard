5b989d72c4fba41141cf6bb8da358670b0c6ff8e
destroyedlolo <l_faillie@yahoo.com>
2024-11-12 22:03:24 +0100

    Redefine usefull characters

---
+++ SeleniteLCD/Support/10_AddChar.lua
@@ -0,0 +1,45 @@
+-- Define custom characters
+-- char 0-2 are already used by the clock
+
+lcd:SetChar(3, {
+	"     ",
+	"  XXx",
+	" xxx ",
+	"xxx  ",
+	"  xx ",
+	" xx  ",
+	"x    ",
+	"     "
+})
+
+lcd:SetChar(4, {
+	"     ",
+	" x x ",
+	"xxxxx",
+	"xxxxx",
+	"xxxxx",
+	" xxx ",
+	"  x  ",
+	"  x  "
+})
+
+lcd:SetChar(5, {
+	" xXX ",
+	"x   X",
+	"XXX  ",
+	"X    ",
+	"XXX  ",
+	"x   X",
+	" xXx ",
+	"     "
+})
+
+--[[
+lcd:SetCursor(0,1)
+lcd:WriteString("New : ")
+for j = 0x08, 0x0f do
+	lcd:WriteString( string.char(j) )
+end
+
+io.stdin:read'*l'       -- wait for enter
+--]]
